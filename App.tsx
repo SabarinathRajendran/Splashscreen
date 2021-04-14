8a7249e9eb6ca7d2af1c34b3ef1198f36581cea5
Sabainath Rajendran <sabarinathkrajenderan@gmail.com>
2021-04-14 20:00:02 +0530

    hello

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { OnBoarding } from "./src/screens/Authentication";
+
+const AuthenticationStack = createStackNavigator();
+
+/*const fonts = {
+  "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+};
+*/
+
+const AuthenticationNavigator = () => {
+  return (
+    <AuthenticationStack.Navigator headerMode="none">
+      <AuthenticationStack.Screen name="OnBoarding" component={OnBoarding} />
+    </AuthenticationStack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthenticationNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

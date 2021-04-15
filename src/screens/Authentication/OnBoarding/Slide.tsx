212a5001bfd35eb927479cc869c03f69fc24fdb0
Sabainath Rajendran <sabarinathkrajenderan@gmail.com>
2021-04-15 13:27:22 +0530

    gfh

---
+++ src/screens/Authentication/OnBoarding/Slide.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+
+interface SlideProps {
+  label: string;
+  right?: boolean;
+}
+
+const { width, height } = Dimensions.get("window");
+
+const SLIDER_HEIGHT = 0.5 * height;
+
+const Slide = ({ label, right }: SlideProps) => {
+  const transform = [
+    {
+      translateY: SLIDER_HEIGHT / 2,
+    },
+    {
+      translateX: right ? width / 2 - 50 : -width / 2 + 50,
+    },
+    {
+      rotate: right ? "-90deg" : "90deg",
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <View style={{ ...styles.titlecontainer, transform }}>
+        <Text style={styles.title}>{label}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Slide;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width,
+  },
+  titlecontainer: {
+    height: "auto",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 80,
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#ffffff",
+  },
+});
